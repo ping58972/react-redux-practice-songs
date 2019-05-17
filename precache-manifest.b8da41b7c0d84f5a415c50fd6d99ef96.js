51c3d26e3cdecdb5b396592774ebc0eac8ca0994
ping58972 <ping58972@gmail.com>
2019-05-17 15:58:50 -0500

    Updates

---
+++ precache-manifest.b8da41b7c0d84f5a415c50fd6d99ef96.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8b68b8f71e16dc80207b2da9e487612",
+    "url": "/react-redux-practice-songs/index.html"
+  },
+  {
+    "revision": "ca03d27ad241a21f33a3",
+    "url": "/react-redux-practice-songs/static/js/2.b9c01a8d.chunk.js"
+  },
+  {
+    "revision": "a0eaab713cf285a7c464",
+    "url": "/react-redux-practice-songs/static/js/main.3c428578.chunk.js"
+  },
+  {
+    "revision": "4f896a5e1d61e544b542",
+    "url": "/react-redux-practice-songs/static/js/runtime~main.92b06f11.js"
+  }
+]);
